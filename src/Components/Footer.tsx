4de4ce83a5df89617f5332ba8535b67ffcaaac65
Mehak bansal <mehak1921@yahoo.com>
2025-02-08 17:48:32 +0530

    fix: update styling a bit

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-        <footer className="absolute w-full h-35 py-3 pl-3 bg-black bottom-0" >
+        <footer className="absolute w-full h-auto py-3 px-3 bg-black bottom-0" >
             <div className="flex flex-row">
-                <img src="#" alt="logo" className="w-100 h-50" />
+                <img src="#" alt="logo" className="w-100 h-auto" />
             </div>
             <div className="text-white text-center text-xs">&copy; Copyright</div>
         </footer>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,11 +2,11 @@ import Nav from "./Nav";
 
 export default function Header() {
     return (
-        <header className="w-full flex h-35 py-3 pl-3 bg-black" >
+        <header className="w-full flex h-auto py-3 px-3 bg-black md:h-10" >
             <div className="flex w-full flex-row md:w-8/12 lg:w-9/12 2xl:w-10/12">
                 <img src="#" alt="logo" className="w-full md:w-48" />
             </div>
-            <ul className="md:w-4/12 lg:w-3/12 2xl:w-2/12 flex flex-row items-center justify-between">
+            <ul className="md:w-4/12 lg:w-4/12 2xl:w-2/12 flex flex-row items-center justify-between">
                 <Nav />
             </ul>
         </header>
